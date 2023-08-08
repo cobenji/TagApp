1655dc88a39d9c8f0eb185a55c4e0e8d1351f15e
Cobenji <>
2023-08-08 16:33:45 +0200

    show map

---
+++ lib/cons.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken = 'pk.eyJ1IjoiY29jb2JlbjE0MDgiLCJhIjoiY2t3cWVtOWI0MDVpMjJ1bng3d3JpencxaSJ9.YnCB20rS5IDjBsgg8H0Z7g';
+
+  static const String mapBoxStyleId = 'cll2ceyhe00d101pbd2rrck5c';
+
+  static final myLocation = LatLng(51.5090214, -0.1982948);
+}
